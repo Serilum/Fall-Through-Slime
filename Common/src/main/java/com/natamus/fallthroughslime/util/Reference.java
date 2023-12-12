f82545f59ac412eead20322e5e2c2bc9fafece0c
Rick South <rick@natamus.com>
2023-12-12 10:17:10 +0100

    Fall Through Slime source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/fallthroughslime/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.fallthroughslime.util;
+
+public class Reference {
+	public static final String MOD_ID = "fallthroughslime";
+	public static final String NAME = "Fall Through Slime";
+	public static final String VERSION = "3.0";
+	public static final String ACCEPTED_VERSIONS = "[1.19.4]";
+}
